77803c6f017047ec56f612b625685f3a98fae783
Kyle Smith <kylesmith0630@gmail.com>
2019-02-12 20:20:39 -0600

    first commit

---
+++ Diabetes ApplicationTests/Diabetes_ApplicationTests.swift
@@ -0,0 +1,34 @@
+//
+//  Diabetes_ApplicationTests.swift
+//  Diabetes ApplicationTests
+//
+//  Created by Kyle Smith on 2/12/19.
+//  Copyright © 2019 Kyle Smith. All rights reserved.
+//
+
+import XCTest
+@testable import Diabetes_Application
+
+class Diabetes_ApplicationTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
